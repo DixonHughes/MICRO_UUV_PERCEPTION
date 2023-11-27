9f9502472125d88841a00114c3b8daf8c76370d0
DixonHughes <107696343+DixonHughes@users.noreply.github.com>
2023-11-27 09:46:27 -0500

    Update scanplot.py
    Initial visual reference test plot (not time dependent)

--- scanplot.py
+++ scanplot.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#c = [68, 138, 188, 219, 245, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255] #color values from 1 ping 
-c = open('test_1.txt','r')
-color_data = c.read()  #color intensity dependant on range 
-angle = np.arange(200)
-x = color_data.size
-print(x)
-plt.scatter(angle,color_data)
-#plt.scatter()
+import matplotlib as mpl
+from matplotlib.colors import Normalize
+from matplotlib.text import TextPath
+
+color = []                                 #this for loop creates a list from the txt file
+for line in open("test_1.txt"):
+    nums = line.split()
+    nums = map(int, nums)
+
+    color.extend(nums)
+
+#Ecolor = int(color())
+print(type(color[0]))
+angle = np.arange(200)                  #this is the angle in gradians per sonar ping, 400 gradians andd only 1216 returns
+y = range(0,200,1)
+
+fig, ax = plt.subplots()
+cmap = plt.colormaps["viridis"]
+plt.scatter(angle , y, c = color)
+fig.suptitle("Sonar Intensity Returns", size = 13) 
+ax.set_xlabel("Sonar Angle (gradians)")
+ax.set_ylabel("Sonar Range (element-wise)")
+plt.colorbar()
 plt.show()
-#print(color_data)
-#print(angle)
